e567063c336223506c462c0b15b2b481ea7e2ba3
AleksKislov <sinokislov@gmail.com>
2020-11-04 13:07:57 +0300

    Add phrases to db

--- app.js
+++ app.js
@@ -1,18 +1,63 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
+const sqlite3 = require("sqlite3").verbose();
+const axios = require("axios");
 const app = express();
-const { getPhrase } = require('./services');
+const { getPhrase } = require("./services");
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('hello there!'));
+const { BOT_INFO, CHAT_ID } = process.env;
 
-app.get('/get_it', async (req, res) => {
-  //length 25
+let db = new sqlite3.Database("./db/phrases.db", err => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log("Connected to the phrases database.");
+});
+
+app.get("/", (req, res) => res.send("hello there!"));
+
+/**
+ * return { chinese, pinyin, english, name }
+ */
+app.get("/get_it/:phraseType", async (req, res) => {
+  const { phraseType } = req.params;
   try {
-    const obj = await getPhrase('sentence');
+    let obj = await getPhrase(phraseType);
+
+    // if (obj && obj.chinese.length > 25) {
+    //   obj = await getPhrase("proverb");
+    // }
+
+    const { chinese, pinyin, english, name } = obj;
+    // const phrase = `📚 ${chinese}\n💬 <i>${pinyin}</i>\n♻️ ${english} \n[ <b>${name}</b> ]`;
+
+    // await axios.get(
+    //   `https://api.telegram.org/${BOT_INFO}/sendMessage?text=${encodeURI(
+    //     phrase
+    //   )}&chat_id=${CHAT_ID}&parse_mode=HTML`
+    // );
+
+    // insert one row into the langs table
+    db.run(
+      `INSERT INTO phrases( chinese, pinyin, english, origin ) VALUES(?,?,?,?)`,
+      [chinese, pinyin, english, name],
+      function(err) {
+        if (err) {
+          return console.log(err.message);
+        }
+        // get the last insert id
+        console.log(`A row has been inserted with rowid ${this.lastID}`);
+      }
+    );
+
+    // close the database connection
+    // db.close();
+
     res.json(obj);
   } catch (error) {
     console.log(error);
-    res.json({ err: 'Oops, something went wrong!' });
+    res.json({ err: "Oops, something went wrong!" });
   }
 });
 
--- services/getPhrase.js
+++ services/getPhrase.js
@@ -1,25 +1,25 @@
-const puppeteer = require('puppeteer');
-require('dotenv').config();
+const puppeteer = require("puppeteer");
+require("dotenv").config();
 
 const _myStore = {
   page: null,
-  url: process.env.WEBSITE_URL,
+  url: process.env.WEBSITE1_URL,
   proverb: {
     xPathChin: process.env.PROVERB_CHIN,
     xPathPinyin: process.env.PROVERB_PINYIN,
-    xPathEng: process.env.PROVERB_ENG,
+    xPathEng: process.env.PROVERB_ENG
   },
   sentence: {
     xPathChin: process.env.SENT_CHIN,
     xPathPinyin: process.env.SENT_PINYIN,
     xPathEng: process.env.SENT_ENG,
-    xPathName: process.env.SENT_NAME,
-  },
+    xPathName: process.env.SENT_NAME
+  }
 };
 
 const extractText = async (xPath, isName) => {
   const [el] = await _myStore.page.$x(xPath);
-  const textContent = await el.getProperty('textContent');
+  const textContent = await el.getProperty("textContent");
   const raw = await textContent.jsonValue();
   if (isName) return raw.slice(2).trim();
   return raw.trim();
@@ -28,16 +28,16 @@ const extractText = async (xPath, isName) => {
 /**
  * @param {string} phraseType - proverb or sentence
  */
-const getPhrase = async (phraseType) => {
-  if (phraseType === 'proverb') {
+const getPhrase = async phraseType => {
+  if (phraseType === "proverb") {
     var {
       url,
-      proverb: { xPathChin, xPathPinyin, xPathEng },
+      proverb: { xPathChin, xPathPinyin, xPathEng }
     } = _myStore;
-  } else if (phraseType === 'sentence') {
+  } else if (phraseType === "sentence") {
     var {
       url,
-      sentence: { xPathChin, xPathPinyin, xPathEng, xPathName },
+      sentence: { xPathChin, xPathPinyin, xPathEng, xPathName }
     } = _myStore;
   }
 
@@ -47,21 +47,20 @@ const getPhrase = async (phraseType) => {
     _myStore.page = page;
     await page.goto(url);
 
-    const path1 = page.waitForXPath(xPathChin, 3000);
-    const path2 = page.waitForXPath(xPathPinyin, 3000);
-    const path3 = page.waitForXPath(xPathEng, 3000);
-    if (phraseType === 'sentence')
-      var path4 = page.waitForXPath(xPathName, 3000);
+    const path1 = page.waitForXPath(xPathChin, 5000);
+    const path2 = page.waitForXPath(xPathPinyin, 5000);
+    const path3 = page.waitForXPath(xPathEng, 5000);
+    if (phraseType === "sentence") var path4 = page.waitForXPath(xPathName, 5000);
     await Promise.all([path1, path2, path3, path4]);
 
     const chinese = await extractText(xPathChin);
     const english = await extractText(xPathEng);
     const pinyin = await extractText(xPathPinyin);
-    if (phraseType === 'sentence') {
+    if (phraseType === "sentence") {
       var name = await extractText(xPathName);
       name = name.slice(1).trim();
-    } else if (phraseType === 'proverb') {
-      var name = 'пословица';
+    } else if (phraseType === "proverb") {
+      var name = "пословица";
     }
     await browser.close();
 
