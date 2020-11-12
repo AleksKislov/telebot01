e9abccd38075cbce5ed62b5d1f7c048af1a5845c
AleksKislov <sinokislov@gmail.com>
2020-11-12 19:26:14 +0300

    Create image service more or less done

--- services/createImage.js
+++ services/createImage.js
@@ -3,9 +3,9 @@ const { createCanvas, loadImage } = require("canvas");
 
 const width = 1000;
 const height = 1000;
-const zhTextHeight = 0.27; // percent from heght
-const pinyinHeight = 0.4;
-const ruTextHeight = 0.57;
+const zhTextHeight = height * 0.27;
+const pinyinHeight = height * 0.42;
+const ruTextHeight = height * 0.57;
 const chineseFont = "bold 40pt Menlo"; // max 16 chars for line
 const pinyinFont = "bold 24pt Menlo"; // max 42 chars
 const rusFont = "20pt Menlo"; // max 52 chars
@@ -20,47 +20,68 @@ const createBackground = () => {
   context.fillRect(0, 0, width, height);
 };
 
-// max line length == 16
 const writeChinese = chineseText => {
+  const zh_length = chineseText.length;
   context.font = chineseFont;
-  context.fillText(chineseText, width / 2, height * zhTextHeight);
+
+  if (zh_length > 32) throw new Error("Слишком длинный китайский текст!");
+
+  if (zh_length > 16) {
+    const halfInd = Math.ceil(zh_length / 2) + 2;
+    context.fillText(chineseText.slice(0, halfInd), width / 2, zhTextHeight);
+    context.fillText(chineseText.slice(halfInd), width / 2, zhTextHeight + 60);
+  } else {
+    context.fillText(chineseText, width / 2, zhTextHeight);
+  }
 };
 
 const writePinyin = pinyin => {
   context.font = pinyinFont;
-  if (pinyin.length > 40) {
+  if (pinyin.length > 42) {
     pinyin = pinyin.split(" ");
-    const halfInd = Math.ceil(pinyin.length / 2);
-    context.fillText(pinyin.slice(0, halfInd).join(" "), width / 2, height * pinyinHeight);
-    context.fillText(pinyin.slice(halfInd).join(" "), width / 2, height * pinyinHeight + 50);
+    const halfInd = Math.ceil(pinyin.length / 2) + 1;
+    context.fillText(pinyin.slice(0, halfInd).join(" "), width / 2, pinyinHeight);
+    context.fillText(pinyin.slice(halfInd).join(" "), width / 2, pinyinHeight + 45);
   } else {
-    context.fillText(pinyin, width / 2, height * pinyinHeight);
+    context.fillText(pinyin, width / 2, pinyinHeight);
   }
 };
 
 const writeRussian = russianText => {
   context.font = rusFont;
-  context.fillText(russianText, width / 2, height * ruTextHeight);
+  if (russianText.length > 52) {
+    russianText = russianText.split(" ");
+    const halfInd = Math.ceil(russianText.length / 2) + 1;
+    context.fillText(russianText.slice(0, halfInd).join(" "), width / 2, ruTextHeight);
+    context.fillText(russianText.slice(halfInd).join(" "), width / 2, ruTextHeight + 40);
+  } else {
+    context.fillText(russianText, width / 2, ruTextHeight);
+  }
 };
 
-// createBackground();
+const writeFooter = () => {
+  const footerText = `каждый день с озвучкой в телеграме:
+      t.me/chineseplusnew`;
+  context.font = "16pt Menlo";
+  context.fillText(footerText, width * 0.54, height * 0.89);
+};
 
-loadImage("../images/backgrounds/bg04.png").then(image => {
-  context.drawImage(image, 0, 0, 1000, 1000);
+const createImage = ({ chinese, pinyin, russian, id }) => {
+  loadImage("../images/backgrounds/bg04.png").then(image => {
+    context.drawImage(image, 0, 0, 1000, 1000);
+    writeChinese(chinese);
+    writePinyin(pinyin);
+    writeRussian(russian);
+    writeFooter();
+  });
 
-  writeChinese("说起来容易，做起来难说起来容易");
-  writePinyin("shuōqǐlái róngyì, zuòqǐlái nán");
-  writeRussian(`Сказать легко, а сделать - сложно.`);
-});
+  loadImage("../images/backgrounds/logo.png").then(image => {
+    context.drawImage(image, width * 0.25, height * 0.87, 50, 50);
+    const buffer = canvas.toBuffer("image/png");
+    fs.writeFileSync(`../images/created/phrases/phrase_${id}.png`, buffer);
+  });
+};
 
-// const footerText = `каждый день с озвучкой в телеграме:
-//       t.me/chineseplusnew`;
-// context.fillStyle = "#fff";
-// context.font = "16pt Menlo";
-// context.fillText(footerText, 630, 530);
+// createImage({ chinese: "sdsd", pinyin: "sdas", russian: "dds", id: 13 });
 
-loadImage("../images/backgrounds/logo.png").then(image => {
-  context.drawImage(image, 330, height * 0.83, 70, 70);
-  const buffer = canvas.toBuffer("image/png");
-  fs.writeFileSync("../images/created/test.png", buffer);
-});
+module.exports = createImage;
--- services/index.js
+++ services/index.js
@@ -1,5 +1,6 @@
 const { getPhrase } = require("./getPhrase");
 const { getTranslation } = require("./getTranslation");
 const { getIdiom } = require("./getIdiom");
+const createImage = require("./createImage");
 
-module.exports = { getPhrase, getTranslation, getIdiom };
+module.exports = { getPhrase, getTranslation, getIdiom, createImage };
